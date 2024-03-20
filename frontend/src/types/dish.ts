0a1b48cd43ba398fac3091c757fb84651f3ebcce
Yuri Cruger <aoddrewiid@gmail.com>
2024-03-20 17:55:06 -0300

    feat: adicionando tipagem para o prato

--- frontend/src/types/dish.ts
+++ frontend/src/types/dish.ts
@@ -5,6 +5,15 @@ export interface Dish {
   category: string;
   price: number;
   image: string;
+  ingredients: Ingredient[];
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Ingredient {
+  id: number;
+  name: string;
+  dish_id: number;
   created_at: string;
   updated_at: string;
 }
