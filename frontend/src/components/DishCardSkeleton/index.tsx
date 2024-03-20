29d5cff156b2e944c51a619b234b439ee9c7efcc
Yuri Cruger <aoddrewiid@gmail.com>
2024-03-20 17:59:44 -0300

    feat: utilizando o hook useDish na home e criando o compornente de skeleton para os cards

---
+++ frontend/src/components/DishCardSkeleton/index.tsx
@@ -0,0 +1,5 @@
+export function DishCardSkeleton() {
+  return (
+    <div className="relative bg-light-500 animate-pulse w-[210px] h-[292px] rounded-lg ring-2 ring-dark-100 lg:w-[304px] lg:h-[462px]" />
+  );
+}
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,38 +1,38 @@
-import { useEffect, useState } from "react";
-import { api } from "@/services/api";
 import { BannerHome } from "@/components/BannerHome";
 import { SectionTitle } from "@/components/SectionTitle";
 import { DishCard } from "@/components/DishCard";
-import { Dish } from "@/types/dish";
+import { useDish } from "@/hooks/dishes";
+import { DishCardSkeleton } from "@/components/DishCardSkeleton";
 
 export default function Home() {
-  const [dishes, setDishes] = useState<Dish[]>([]);
+  const { dishList, fetchErrorOccurred } = useDish();
 
-  useEffect(() => {
-    api
-      .get("/dishes")
-      .then((response) => {
-        setDishes(response.data);
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
-  }, []);
+  if (fetchErrorOccurred) {
+    alert(
+      "Ocorreu um erro ao buscar os pratos. Por favor, tente novamente mais tarde."
+    );
+  }
 
   return (
     <>
       <BannerHome />
       <SectionTitle title="Refeições" />
 
-      <div className="flex gap-5 px-7">
-        {dishes.map((dish) => (
-          <DishCard
-            key={dish.id}
-            name={dish.name}
-            price={dish.price}
-            img={dish.image}
-          />
-        ))}
+      <div className="flex gap-5 px-7 mb-12 xl:px-32">
+        {dishList
+          ? dishList.map((dish) => (
+              <DishCard
+                key={dish.id}
+                name={dish.name}
+                price={dish.price}
+                img={dish.image}
+                id={dish.id}
+                ingredients={dish.ingredients}
+              />
+            ))
+          : Array(5)
+              .fill(0)
+              .map((_, index) => <DishCardSkeleton key={index} />)}
       </div>
     </>
   );
