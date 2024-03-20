54730a49ba94c970bdbdc1e72cd559d7b93c85fa
Yuri Cruger <aoddrewiid@gmail.com>
2024-03-20 18:00:23 -0300

    feat: envolvendo a aplicação sobre o provider DishProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Routes } from "./routes";
 import { AuthProvider } from "./hooks/auth";
+import { DishProvider } from "./hooks/dishes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <Routes />
+      <DishProvider>
+        <Routes />
+      </DishProvider>
     </AuthProvider>
   </React.StrictMode>
 );
